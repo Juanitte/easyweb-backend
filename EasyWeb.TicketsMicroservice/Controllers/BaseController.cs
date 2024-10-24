62f707524337254a4cb098a12110f90c9a0ba0d0
Juanite <juanite.dev@gmail.com>
2024-10-24 21:58:44 +0200

    Completed TicketsMicroservice

---
+++ EasyWeb.TicketsMicroservice/Controllers/BaseController.cs
@@ -0,0 +1,37 @@
+﻿using EasyWeb.TicketsMicroservice.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyWeb.TicketsMicroservice.Controllers
+{
+    public class BaseController : ControllerBase
+    {
+        #region Miembros privados
+
+        public readonly IServiceProvider _serviceCollection;
+
+        #endregion
+
+        #region Miembros internos
+
+        internal ILogger IoTLogger => (ILogger)_serviceCollection.GetService(typeof(ILogger));
+        internal IConfiguration Configuration => (IConfiguration)_serviceCollection.GetService(typeof(IConfiguration));
+        internal ITicketsService IoTServiceTickets => (ITicketsService)_serviceCollection.GetService(typeof(ITicketsService));
+        internal IMessagesService IoTServiceMessages => (IMessagesService)_serviceCollection.GetService(typeof(IMessagesService));
+        internal IAttachmentsService IoTServiceAttachments => (IAttachmentsService)_serviceCollection.GetService(typeof(IAttachmentsService));
+
+        #endregion
+
+        #region Constructores
+
+        /// <summary>
+        ///     Constructor base, almacena el service collection
+        /// </summary>
+        /// <param name="serviceCollection"></param>
+        public BaseController(IServiceProvider serviceCollection)
+        {
+            _serviceCollection = serviceCollection;
+        }
+
+        #endregion
+    }
+}
--- EasyWeb_Microservice_User/Translations/Translation_Errors.Designer.cs
+++ EasyWeb_Microservice_User/Translations/Translation_Errors.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Tickets.UsersMicroservice.Translations {
+namespace EasyWeb.UsersMicroservice.Translations {
     using System;
     
     
